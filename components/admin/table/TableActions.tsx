c2f296eb563681a66cdfa0d2912d9a39a082960e
ZY1YOGI <zy1yogi@gmail.com>
2023-09-04 00:28:28 +0300

    fix spaces

--- components/admin/table/TableActions.tsx
+++ components/admin/table/TableActions.tsx
@@ -3,7 +3,7 @@ import { MdOutlineEdit, MdOutlineRemoveRedEye } from "react-icons/md";
 
 export default function TableActions() {
   return (
-    <td className="flex justify-center space-x-3">
+    <td className="flex justify-center space-x-2.5">
       <button className="w-5 transition-all hover:text-green-700 hover:scale-125 md:w-6">
         <MdOutlineRemoveRedEye className="w-5 md:w-7" />
       </button>
