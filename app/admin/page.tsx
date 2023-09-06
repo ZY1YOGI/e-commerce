99150e402493d1ac98ec77074d9d96b1171b59f8
ZY1YOGI <zy1yogi@gmail.com>
2023-09-07 02:43:54 +0300

    work in admin/HomePage

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,29 @@
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main>
-      <div className='container'>
-        <h1 className="b-t w-full">Home</h1>
+    <main className='container space-y-12'>
+
+      <div className="grid grid-cols-4 gap-6 max-md:grid-cols-2">
+        <div className="w-full h-36 rounded-lg shadow-lg shadow-slate-500"></div>
+        <div className="w-full h-36 rounded-lg shadow-lg shadow-slate-500"></div>
+        <div className="w-full h-36 rounded-lg shadow-lg shadow-slate-500"></div>
+        <div className="w-full h-36 rounded-lg shadow-lg shadow-slate-500"></div>
       </div>
+
+      <div className="flex justify-between gap-6 max-md:flex-col">
+        <div className="w-full h-80 rounded-lg shadow-lg shadow-slate-500"></div>
+        <div className="w-full h-80 rounded-lg shadow-lg shadow-slate-500"></div>
+      </div>
+
+      <div className="flex justify-between gap-6 max-md:flex-col">
+        <div className="w-full h-60  flex gap-6">
+          <div className="w-full h-full rounded-lg shadow-lg shadow-slate-400"></div>
+          <div className="w-full h-full rounded-lg shadow-lg shadow-slate-400"></div>
+        </div>
+        <div className="w-full h-60 rounded-lg shadow-lg shadow-slate-500"></div>
+      </div>
+
+
     </main>
   )
 }
