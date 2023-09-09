62d1a90395150bde408c78eeb2fd5e27a270f834
ZY1YOGI <zy1yogi@gmail.com>
2023-09-09 21:44:51 +0300

    fix Head im RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import Head from 'next/head'
+// import Head from 'next/head'
 
 export const metadata: Metadata = {
 
@@ -9,9 +9,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="light" data-theme="light">
-      <Head>
-        {/* <link rel="canonical" href="https://ma-holiday.vercel.app" /> */}
-      </Head>
+      {/* <Head>
+        <link rel="canonical" href="" />
+      </Head> */}
       <body className="antialiased bg-body-light dark:bg-body-dark">
         {children}
       </body>
